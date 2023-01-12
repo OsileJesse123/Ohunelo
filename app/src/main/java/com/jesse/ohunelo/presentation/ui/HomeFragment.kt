7bae60136831d036d701f632f987a1d30c3cdeae
OsileJesse123 <jesseosile@gmail.com>
2023-01-12 20:03:49 +0100

    home screen about to be added

---
+++ app/src/main/java/com/jesse/ohunelo/presentation/ui/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.jesse.ohunelo.presentation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.jesse.ohunelo.R
+import com.jesse.ohunelo.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container,
+            false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/jesse/ohunelo/presentation/ui/OhuneloActivity.kt
+++ app/src/main/java/com/jesse/ohunelo/presentation/ui/OhuneloActivity.kt
@@ -7,6 +7,7 @@ import com.jesse.ohunelo.R
 import com.jesse.ohunelo.databinding.ActivityOhuneloBinding
 
 class OhuneloActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityOhuneloBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
