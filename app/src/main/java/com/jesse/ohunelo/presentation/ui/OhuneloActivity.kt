0ff57a534213a4fee00fc324f6f6385c633f012d
OsileJesse123 <jesseosile@gmail.com>
2023-01-17 22:04:42 +0100

    Navigation graph was added. Little edits were made to styles

--- app/src/main/java/com/jesse/ohunelo/presentation/ui/OhuneloActivity.kt
+++ app/src/main/java/com/jesse/ohunelo/presentation/ui/OhuneloActivity.kt
@@ -3,9 +3,14 @@ package com.jesse.ohunelo.presentation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.jesse.ohunelo.R
 import com.jesse.ohunelo.databinding.ActivityOhuneloBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class OhuneloActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityOhuneloBinding
@@ -16,6 +21,15 @@ class OhuneloActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityOhuneloBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.ohunelo_fragment_container)
+                as NavHostFragment
+        val navController = navHost.navController
+        binding.ohuneloBottomNav.setupWithNavController(navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.ohunelo_fragment_container)
+        return navController.navigateUp()
+    }
 }
