06084b22764d84be7757fda321524c20410ba5ed
OsileJesse123 <jesseosile@gmail.com>
2023-08-17 16:51:58 +0100

    Profile screen added

---
+++ app/src/main/java/com/jesse/ohunelo/presentation/ui/fragment/ProfileFragment.kt
@@ -0,0 +1,33 @@
+package com.jesse.ohunelo.presentation.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.jesse.ohunelo.R
+import com.jesse.ohunelo.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding: FragmentProfileBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_profile, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
