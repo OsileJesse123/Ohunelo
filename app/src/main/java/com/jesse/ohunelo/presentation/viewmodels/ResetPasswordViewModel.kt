28785a3435ff4da6a0dcac6a5ffe91a09812fc75
OsileJesse123 <jesseosile@gmail.com>
2023-09-01 13:52:49 +0100

    Reset Password Validation Modified - Debounce logic was added for email validation in ResetPasswordViewModel

--- app/src/main/java/com/jesse/ohunelo/presentation/viewmodels/ResetPasswordViewModel.kt
+++ app/src/main/java/com/jesse/ohunelo/presentation/viewmodels/ResetPasswordViewModel.kt
@@ -1,12 +1,16 @@
 package com.jesse.ohunelo.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jesse.ohunelo.domain.usecase.ValidateEmailUseCase
 import com.jesse.ohunelo.presentation.uistates.ResetPasswordUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,14 +21,20 @@ class ResetPasswordViewModel @Inject constructor(
     private val _resetPasswordUiStateFlow: MutableStateFlow<ResetPasswordUiState> = MutableStateFlow(ResetPasswordUiState())
     val resetPasswordUiStateFlow get() = _resetPasswordUiStateFlow.asStateFlow()
 
+    private var validationJob: Job? = null
+
     fun onEmailTextChanged(emailText: String){
-        _resetPasswordUiStateFlow.update {
-                resetPasswordUiState ->
-            val emailValidation = validateEmailUseCase(emailText)
-            resetPasswordUiState.copy(
-                email = emailText,
-                emailError = emailValidation.errorMessage
-            )
+        validationJob?.cancel()
+        validationJob = viewModelScope.launch {
+            delay(500L)
+            _resetPasswordUiStateFlow.update {
+                    resetPasswordUiState ->
+                val emailValidation = validateEmailUseCase(emailText)
+                resetPasswordUiState.copy(
+                    email = emailText,
+                    emailError = emailValidation.errorMessage
+                )
+            }
         }
     }
 
