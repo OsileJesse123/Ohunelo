f78c563b1d06cc2ff64d208ffe67ec0ddfe58164
Osile Jesse <jesseosile@gmail.com>
2024-04-15 11:01:04 +0100

    update profile screen begun - UI layer is being setup for re-authentication.

--- app/src/main/java/com/jesse/ohunelo/presentation/uistates/EditEmailUiState.kt
+++ app/src/main/java/com/jesse/ohunelo/presentation/uistates/EditEmailUiState.kt
@@ -1,6 +1,7 @@
 package com.jesse.ohunelo.presentation.uistates
 
 import com.jesse.ohunelo.util.UiText
+import com.jesse.ohunelo.util.UserType
 
 data class EditEmailUiState(
     val email: String = "",
@@ -8,7 +9,8 @@ data class EditEmailUiState(
     val isEnabled: Boolean = false,
     val isLoading: Boolean = false,
     val message: UiText? = null,
-    val navigateBack: Boolean = false
+    val navigateBack: Boolean = false,
+    val reauthenticate: Pair<Boolean, UserType?> = Pair(false, null)
 ){
     // email is valid if it is not empty and emailError is null
     fun isEmailValid() = (email.isNotEmpty() && emailError == null)
