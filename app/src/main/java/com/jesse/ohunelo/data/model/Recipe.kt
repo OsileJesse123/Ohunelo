c5ef9dad688f1d0d8ea015cf6236938bb79ac50e
Osile Jesse <jesseosile@gmail.com>
2024-04-22 21:06:46 +0100

    update profile screen begun - UI layer is being setup for email update and re-authentication.

--- app/src/main/java/com/jesse/ohunelo/data/model/Recipe.kt
+++ app/src/main/java/com/jesse/ohunelo/data/model/Recipe.kt
@@ -7,7 +7,7 @@ import com.jesse.ohunelo.data.network.models.ExtendedIngredient
 import com.jesse.ohunelo.util.UiText
 import kotlinx.parcelize.Parcelize
 
-//
+// recipe
 @Parcelize
 data class Recipe(
     val id: Int = 0,
